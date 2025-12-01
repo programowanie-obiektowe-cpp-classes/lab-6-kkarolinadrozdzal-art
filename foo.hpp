e1fd28de42fea18e6ec10f4a2916a53bbbb19012
kkarolinadrozdzal-art <k.karolinadrozdzal@gmail.com>
2025-12-01 19:43:42 +0100

    dziala

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,16 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& h) { h.birthday(); });
+    std::transform(people.rbegin(), people.rend(), retval.begin(), [](Human& h) {
+        if (h.isMonster())
+        {
+            return 'n';
+        }
+        else {
+            return 'y';
+        }
+    });
 
     return retval;
 }
